182489f738062cb77191514fa4e59327b5902297
Konstantim <const.trofimov@gmail.com>
2021-02-25 01:23:58 +0300

    updated

--- src/components/v-traffic-light-img.vue
+++ src/components/v-traffic-light-img.vue
@@ -1,12 +1,14 @@
 <template>
 	<div>
 		<img 
+		class="traffic-light-img"
 		:src="require(`../assets/images/${trafficLight}`)"  
 		alt="traffic-light" >
 	</div>
 </template>
 
-<<script>
+<script>
+
 export default {
 	name: 'v-traffic-light',
 	data() {
@@ -17,8 +19,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-	img {
+<style lang="scss">
+	.traffic-light-img {
 		z-index: 0;
 		position: absolute;
 		left: 50%;
@@ -27,31 +29,5 @@ export default {
 		margin: 0 auto;
 		width: 525px;
 		height: 614px;
-	}
-
-	@media (max-width: 600px) and (min-height: 460px)   {
-		img {
-			width: 262.5px;
-			height: 307px;
-		}
-	}
-
-
-
-	@media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape) {
-		img {
-			width: 262.5px;
-			height: 307px;
-		}
-	}
-
-		@media (max-width: 568px) and (max-height: 320px)   {
-		img {
-			width: 131.25px;
-			height: 153.5px;
-		}
-	}
-
-
-	
+	}	
 </style>
